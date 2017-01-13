b5feec073ec94c7034b8ca4cb7394e06bb22496c
Bob Green <bob@wookey.org.uk>
2017-01-13 22:35:05 +0000

    Adding imagebrowser to the image insert button

--- plugin.php
+++ plugin.php
@@ -2,14 +2,14 @@
 
 require_once('pluginplus.php');
 
-class pluginraptorEditor extends PluginPlus
+class pluginTidyMCE extends PluginPlus
 {
   // Controllers that use this plugin. For any other controller, we
   // do nothing
   public $usedBy = ['new-post',
-			      'new-page',
-			      'edit-post',
-			      'edit-page'];
+		    'new-page',
+		    'edit-post',
+		    'edit-page'];
 
   public function adminHead()
   {
